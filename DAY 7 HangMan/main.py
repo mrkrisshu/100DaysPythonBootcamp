949fd46b53568cfd28df847bbb2aab5c241bdfa9
mrkrisshu <mrkrisshu@gmail.com>
2025-06-29 22:47:50 +0530

    2ndtask

--- DAY 7 HangMan/main.py
+++ DAY 7 HangMan/main.py
@@ -6,11 +6,22 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "-"
+print(placeholder)
+
 guess = input("Guess a Letter: ").lower()
-print(guess)
+
+
+display = ""
 
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
+        display += guess
     else :
-        print("Wrong")
+        display += "_"
+
+print(display)
