bd1818a0d8946fa2f6fef0ac0d7487f5cbdcf4c7
mrkrisshu <mrkrisshu@gmail.com>
2025-07-01 23:54:03 +0530

    2ndtask

---
+++ Day8/task.py
@@ -0,0 +1,18 @@
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"what is it like in {location}")
+
+greet_with("Krishna", "Bangalore")
+
+
+
+
+
+
+
+
+
+
+
+
+#keyword arguments
