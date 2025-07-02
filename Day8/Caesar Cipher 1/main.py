bf8adf67f49a62d9e5d192caf1f22ff7f45557a4
mrkrisshu <mrkrisshu@gmail.com>
2025-07-02 23:31:35 +0530

    2ndtask

--- Day8/Caesar Cipher 1/main.py
+++ Day8/Caesar Cipher 1/main.py
@@ -13,8 +13,32 @@ def encrypt(original_text, shift_amount):
 
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
-
-
     print(f"Here is the encoded result:{cipher_text}")
 
+def decrypt(original_text, shift_amount):
+    output_text = ""
+
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+
+    print(f"Here is the decoded result:{output_text}")
+
+def caesar(orginal_text, shift_amount, enocde_or_deocde):
+    output_text = ""
+
+    for letter in original_text:
+
+        if enocde_or_deocde == "decode":
+            shift_amount *= -1
+        shifted_position = alphabet.index(letter) + shift_amount
+
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+
+    print(f"Here is the decoded result:{output_text}")
+
 encrypt(original_text=text, shift_amount=shift)
+decrypt(original_text=text, shift_amount=shift)
